9ec9996e8647003c96c5caeda4d38cf49390c7d5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 21:34:09 -0300

    Rodando o banco  de dados no sistema

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const cors = require("cors")
-const { Sequelize, DataTypes } = require('sequelisze')
+const { Sequelize, DataTypes } = require('sequelize')
 
 // Configuração da conexão com o banco de dados -= MySQL
 const sequelize = new  Sequelize('db_projeto', 'root', '', {
     host: 'localhost',
-    dialect: 'mysqsl'
+    dialect: 'mysql'
 })
 
 // Criando a tabela do banco 
@@ -17,7 +17,7 @@ const Cliente = sequelize.define('Cliente', {
     },
     email:{
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: 'false',
         unique: true
     },
     telefone:{
@@ -25,4 +25,46 @@ const Cliente = sequelize.define('Cliente', {
 
     }
 
+})
+
+// Configuração do servidor express
+const app = express()
+app.use(cors()) // permite o front-end acessa a api
+app.use(express.json()) // perminestate o servidor entender JSON.
+
+const port = 3001
+
+// Definição de rotas (endpoints)
+// req: request
+// res: response
+app.get('/clientes', async (req, res) =>{
+    const todosOsClientes = await Cliente.findAll()
+    res.json(todosOsClientes)
+})
+
+app.post('/clientes', async(req, res) => {
+    try {
+        const {nome, email, telefone} = req.body
+        const novoCliente = await Cliente.create({nome, email, telefone})
+        res.status(201).json({
+            message: 'Cliente cadastrado com sucesso.',
+             cliente: novoCliente
+             })
+        } catch (error) {
+            res.status(400).json({
+                error: 'Erro ao cadastrar cliente. Verifique se o cliente já existe.'
+            })
+
+    }
+})
+
+// Inicializando  o servidor.
+sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`🚀 Servidor rodando na porta: ${port}`)
+        console.log(`😎 Banco de dados sicronizando`)
+    })
+}).catch((error) => {
+    console.error(`❌ erro ao conectar ou sicronizar com o banco de dados`)
+    console.error(error)
 })
